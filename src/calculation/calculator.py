bb4145455550c27e5f5fc0d1b36d2d5de00d7cd3
NaoYamada <NaoYamada@Nao-MacBook-Pro.local>
2023-10-31 23:16:49 +0900

    add fibonacci

--- src/calculation/calculator.py
+++ src/calculation/calculator.py
@@ -1,5 +1,16 @@
-def sum_array(array) -> int:
+def sum_array(arg: list) -> int:
     total = 0
-    for element in array:
+    for element in arg:
         total += element
     return total
+
+
+def fibonacci(n: int) -> int:
+    if n < 0:
+        raise ValueError("n must be >= 0")
+    elif n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n - 2) + fibonacci(n - 1)
--- tests/test_calculation.py
+++ tests/test_calculation.py
@@ -1,6 +1,14 @@
-from calculation.calculator import sum_array
+from calculation.calculator import sum_array, fibonacci
 
 
 def test_sum_array():
     assert sum_array([1, 2, 3, 4, 5]) == 15
     assert sum_array([-1, -2, -3, -4, 1, 2, 3, 4]) == 0
+
+
+def test_fibonacci():
+    assert fibonacci(0) == 0
+    assert fibonacci(1) == 1
+    assert fibonacci(10) == 55
+    assert fibonacci(20) == 6765
+    assert fibonacci(30) == 832040
